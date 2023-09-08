f596e073f7b3ddae2c2f0164a3dc9a8feb1d58b8
Anna Gąsiorowska <annanataliagasiorowska@gmail.com>
2023-09-08 10:42:38 +0200

    remove mockito dependency in test environment

--- src/test/java/com/recruitment/AdminServiceTest.java
+++ src/test/java/com/recruitment/AdminServiceTest.java
@@ -1,49 +1,31 @@
 package com.recruitment;
 
-import com.recruitment.model.ReceiptKind;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
-
-import java.util.HashSet;
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class AdminServiceTest {
-    ExpensesConfig expensesConfig = Mockito.mock(ExpensesConfig.class);
+    ExpensesConfig expensesConfig = new ExpensesConfig();
     AdminService adminService = new AdminService(expensesConfig);
 
     @Test
     void checkIfReceiptKindExists_exists_returnTrue() {
-        Mockito.when(expensesConfig.getReceiptKindSet()).thenReturn(new HashSet<>(List.of(
-                new ReceiptKind("Taxi"),
-                new ReceiptKind("Hotel"),
-                new ReceiptKind("Plain"),
-                new ReceiptKind("Train")
-        )));
         assertTrue(adminService.checkIfReceiptKindExists("Train"));
     }
 
     @Test
     void checkIfReceiptKindExists_doNotExists_returnFalse() {
-        Mockito.when(expensesConfig.getReceiptKindSet()).thenReturn(new HashSet<>(List.of(
-                new ReceiptKind("Taxi"),
-                new ReceiptKind("Hotel"),
-                new ReceiptKind("Plain"),
-                new ReceiptKind("Train")
-        )));
         assertFalse(adminService.checkIfReceiptKindExists("Boat"));
     }
     @Test
     void checkIfReceiptKindExists_emptyString_returnFalse() {
-        Mockito.when(expensesConfig.getReceiptKindSet()).thenReturn(new HashSet<>(List.of(
-                new ReceiptKind("Taxi"),
-                new ReceiptKind("Hotel"),
-                new ReceiptKind("Plain"),
-                new ReceiptKind("Train")
-        )));
         assertFalse(adminService.checkIfReceiptKindExists(""));
+    }
+    @Test
+    void addReceiptKind_properString_returnTrue() {
+        adminService.addReceiptKind("Boat");
+        assertTrue(adminService.checkIfReceiptKindExists("Boat"));
     }
 
 }
